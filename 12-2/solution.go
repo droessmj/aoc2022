fe41802fcf1a8a46b5a99b9ef15b3e3f06ee958d
droessmj <droessmj@gmail.com>
2022-12-02 06:57:07 -0600

    day2 pt2

--- 12-2/solution.go
+++ 12-2/solution.go
@@ -39,6 +39,12 @@ const (
 	Scissors Throw = 3
 )
 
+const (
+	Win int = 6
+	Draw int = 3
+	Lose int = 0
+)
+
 func buildEvalRound(r []string) int {
 	var p1Throw Throw
 	var p2Throw Throw
@@ -84,6 +90,60 @@ func buildEvalRound(r []string) int {
 	return score
 }
 
+func buildEvalRound2(r []string) int {
+	var p1Throw Throw
+	var p2Throw Throw
+	var outcome int
+	var score int = 0 
+
+	switch r[0] {
+		case "A":
+			p1Throw = Rock
+		case "B":
+			p1Throw = Paper
+		case "C": 
+			p1Throw = Scissors
+	}
+
+	switch r[1] {
+		case "X":
+			outcome = Lose
+		case "Y":
+			outcome = Draw
+		case "Z": 
+			outcome = Win
+	}
+
+	switch {
+		case outcome == Draw:
+			score += 3
+			switch p1Throw {
+				case Rock:
+					score += 1
+				case Paper:
+					score += 2
+				case Scissors:
+					score += 3
+			}
+		case p1Throw == Rock && outcome == Lose:
+			score += 3
+		case p1Throw == Rock && outcome == Win:
+			score += 8
+		case p1Throw == Paper && outcome == Lose:
+			score += 1
+		case p1Throw == Paper && outcome == Win:
+			score += 9
+		case p1Throw == Scissors && outcome == Lose:
+			score += 2
+		case p1Throw == Scissors && outcome == Win:
+			score += 7
+	}
+
+	score += int(p2Throw)
+
+	return score
+}
+
 func solvePart1(input [][]string) int {
 
 	var score int = 0 
@@ -95,6 +155,16 @@ func solvePart1(input [][]string) int {
 	return score
 }
 
+func solvePart2(input [][]string) int {
+
+	var score int = 0 
+
+	for _, e := range input {
+		score += buildEvalRound2(e)
+	}
+
+	return score
+}
 
 func main() {
 
@@ -103,7 +173,6 @@ func main() {
 	resultPt1 := solvePart1(inputs)
 	fmt.Println(resultPt1)
 
-	// resultPt2 := solvePart2(input)
-	// fmt.Println(resultPt2)
-
+	resultPt2 := solvePart2(inputs)
+	fmt.Println(resultPt2)
 }
