ac3a0d29510c401974e24e4df4ecdecb866d903f
droessmj <droessmj@gmail.com>
2022-12-03 07:34:57 -0600

    day3 pt2

--- 12-3/solution.go
+++ 12-3/solution.go
@@ -86,6 +86,60 @@ func solvePart1(inputs []string) int {
 	return score
 }
 
+func solvePart2(inputs []string) int {
+	var score int = 0 
+	var groupIdx int = 0 
+
+	//lowercase a == 97 (1) == -96 offset
+	//upercase A == 65 (27) == -38 offset
+	const lowerOffset int = -96
+	const upperOffset int = -38
+
+	set := make(map[rune]int)
+	var exists = struct{}{}
+
+	for _, e := range inputs {
+		seen := make(map[rune]struct{})
+
+		for _, r := range e {
+			// if we've already seen this rune, continue
+			if _, ok := seen[r]; ok  {
+				continue
+			} else {
+				seen[r] = exists
+			}
+
+			if groupIdx== 0 {
+				set[r] = 1
+			} else {
+				if _, ok := set[r]; ok  {
+					set[r]++
+				} 
+			}
+		}
+
+		groupIdx++
+		if groupIdx > 2 {
+			for r, val := range set {
+				if val > 2 {
+					//fmt.Println(r, val)
+					if r < 95 {
+						score += (int(r) + upperOffset)
+					} else {
+						score += (int(r) + lowerOffset)
+					}
+				}
+			}
+
+			groupIdx = 0
+			// set meta-set to null
+			set = make(map[rune]int)
+		}
+	}
+
+	return score
+}
+
 func main() {
 
 	inputs := parseInput()
@@ -93,6 +147,6 @@ func main() {
 	resultPt1 := solvePart1(inputs)
 	fmt.Println(resultPt1)
 
-	// resultPt2 := solvePart2(inputs)
-	// fmt.Println(resultPt2)
+	resultPt2 := solvePart2(inputs)
+	fmt.Println(resultPt2)
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # 2022 Advent of Code
 
 ### Exercises attempted in Go to better learn the language
+
+#### TODO 
+
+- Module / package imports
+- Debugging setup
