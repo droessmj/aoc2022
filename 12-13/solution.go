6bb0c90d27e9d967124f4c294cb1ac8a89d90ebc
droessmj <droessmj@gmail.com>
2022-12-13 06:43:36 -0600

    day13 wip

--- 12-13/solution.go
+++ 12-13/solution.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/emirpasic/gods/stacks/arraystack"
 )
 
-func parseInput() []int {
-	var input []int
+func parseInput() []string {
+	var input []string
 
 	var name string = "input.test"
 	if len(os.Args) > 1 {
@@ -24,11 +27,7 @@ func parseInput() []int {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		if !(len(scanner.Text()) == 0) {
-			scannerInt, _ := strconv.Atoi(scanner.Text())
-			input = append(input, scannerInt)
-
-		} else {
-			input = append(input, -1)
+			input = append(input, scanner.Text())
 
 		}
 	}
@@ -40,11 +39,79 @@ func parseInput() []int {
 	return input
 }
 
-func solvePart1(input []int) int {
-	return 0
+type Packet struct {
+	values []interface{}
 }
 
-func solvePart2(input []int) int {
+func NewPacket(s string) Packet {
+	var packetValues []interface{}
+	var curList []interface{}
+	headerStack := arraystack.New()
+
+	stringPieces := strings.Split(s, ",")
+	for _, piece := range stringPieces {
+		i, ok := strconv.Atoi(string(piece))
+
+		switch {
+
+		case ok == nil:
+			curList = append(curList, i)
+
+		case strings.Contains()'[':
+			curList = make([]interface{}, 0)
+
+		case r[0] == ']':
+			//Pop?
+			packetValues = append(packetValues, curList...)
+
+		default:
+			panic("default hit")
+		}
+	}
+
+	return Packet{values: packetValues}
+}
+
+func (left Packet) LessThanEqualTo(right Packet) bool {
+	for leftIdx, leftVal := range left.values {
+
+		if len(right.values) < leftIdx {
+			return false
+		}
+
+		rightVal := right.values[leftIdx]
+
+		switch leftVal.(type) {
+		case int:
+			if rightVal.(int) < leftVal.(int) {
+				return false
+			}
+
+		case []int:
+		}
+	}
+
+	return true
+}
+
+func solvePart1(input []string) int {
+	var correctCount int = 0
+
+	for i := 0; i < len(input); i += 2 {
+		left := NewPacket(input[i])
+		right := NewPacket(input[i+1])
+
+		if left.LessThanEqualTo(right) {
+			correctCount += (i + 1)
+
+			fmt.Println(left, right, i+1)
+		}
+	}
+
+	return correctCount
+}
+
+func solvePart2(input []string) int {
 
 	return 0
 }
@@ -53,7 +120,7 @@ func main() {
 
 	//fmt.Println(os.Args[1])
 	input := parseInput()
-	fmt.Println(input)
+	//fmt.Println(input)
 
 	resultPt1 := solvePart1(input)
 	fmt.Println(resultPt1)
